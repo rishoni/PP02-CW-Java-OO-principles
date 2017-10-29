3886d49df554bb9e52c1b36fd22154e13b8eabf3
RAJESH D <rajedan@gmail.com>
2017-10-29 08:08:57 +0530

    Added some meaningfull commends across the different apis and class level

--- Hotel Array/src/Room.java
+++ Hotel Array/src/Room.java
@@ -1,22 +1,46 @@
-
+/**
+ * 
+ * This class represent a Room
+ */
 public class Room {
 	String CustomerN;
 	int RoomN;
+
+	/**
+	 * Sets the Customer Number of this#Room to passed CustomerN
+	 * 
+	 * @param CustomerN
+	 */
 	public void setCustomer(String CustomerN) {
 		this.CustomerN = CustomerN;
-		
+
 	}
-	public String getCustomer(){
+
+	/**
+	 * 
+	 * @return CustomerN of String type
+	 */
+	public String getCustomer() {
 		return this.CustomerN;
-		
+
 	}
+
+	/**
+	 * Sets the Room Number of this#Room to passed RoomN
+	 * 
+	 * @param RoomN
+	 */
 	public void setRoom(int RoomN) {
 		this.RoomN = RoomN;
 	}
-	public int getRoom(){
+
+	/**
+	 * 
+	 * @return RoomN of int type
+	 */
+	public int getRoom() {
 		return this.RoomN;
-		
+
 	}
-	
-	
+
 }
